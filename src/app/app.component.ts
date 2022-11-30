24c31697b7a67ab264e5ae1c5001565e2607a305
angelItza <you@example.com>
2022-11-29 23:31:59 -0600

    10-20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  title = 'Listado empleados';
+empleados:Empleado[]=[
+
+new Empleado("Juan","papu","velador",7500),
+new Empleado("Elver","galarga","adasdas",12312),
+
+new Empleado("perez","gomes","nooo",232),
+
+new Empleado("tela","tragas","dash y su disfraz",137289),
+
+
+];
+
+agregarEmpleados(){
+  let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario)
+
+
+  this.empleados.push(miEmpleado);
+}
+
+cuadroNombre:string="";
+cuadroApellido:string="";
+
+cuadroCargo:string="";
+
+cuadroSalario:number=0;
+
+
+
+
+
 }
