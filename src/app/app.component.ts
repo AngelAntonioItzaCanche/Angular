926aa66ba3483523bcea5d737e5e47b8bab857c8
angelItza <you@example.com>
2022-11-29 23:37:15 -0600

    20-30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivasApp';
+  title = 'Registro Usuario';
+
+mensaje="";
+
+registrado=false;
+
+nombre="";
+apellido="";
+cargo:string="";
+
+entradas:any;
+constructor(){
+  this.entradas=[
+    {titulo:"python es chido"},
+    {titulo:"java present"},
+    {titulo:"javascript es zzz"},
+    {titulo:"kotlin es asco"},
+    {titulo:"zmzmzmzmzm"},
+//BAJAR LA ROPA 
+
+
+
+  ]
+} 
+
+registrarUsuario(){
+  this.mensaje="Usuario Registrado con Exito"
+  this.registrado=true;
+}
+
+
 }
